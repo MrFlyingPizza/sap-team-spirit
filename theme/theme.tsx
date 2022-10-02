709157bf00c09d868a2117aaf6947c830e66ad6d
richardgao <rgao473@gmail.com>
2022-10-01 19:44:46 -0700

    added dependencies

---
+++ theme/theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#80deea',
+        },
+        secondary: {
+            main: '#7250ff',
+        },
+        success: {
+            main: '#8de468',
+        },
+    },
+});
+
+export default theme;
